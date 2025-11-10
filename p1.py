7d8f97c59f3f01fb4d5cfe902ea6f4cbae15a3c7
maryam <army1384m@gmail.com>
2025-11-10 22:31:55 +0330

    contine practice

--- p1.py
+++ p1.py
@@ -1,4 +1,9 @@
+import time
 from datetime import datetime
+import colorama
+from colorama import Fore, Back, Style, init
+
+init()
 
 class Report:
     def __init__(self, filename):
@@ -15,7 +20,8 @@ class Report:
 
     
     
-class Machine:   
+class Machine:
+    counter = 0   
     def __init__(self, name):
         self.status = 'stand by'
         self.name = name 
@@ -25,21 +31,73 @@ class Machine:
             if self.status == 'stand by':
                 self.status = 'active'
                 self.time = datetime.now().strftime('%H :%M : %S')
-
+                Machine.counter += 1
+                print(Fore.GREEN + "machine activated!")
             else :
                 raise ValueError
 
         except: 
             print("machine is already active")
 
-        return f"{self.status}:{self.time}:"
-    
+        return f"{self.status}:{self.time}: {self.name}\n"
     
 
+    def pause(self):
+        try:
+            if self.status == 'active':
+                self.status = 'pause'
+                self.time = datetime.now().strftime('%H :%M : %S')
+                Machine.counter += 1
+                print(Fore.BLUE + "machine paused!")
+
+            else :
+                raise ValueError
+
+        except: 
+            print("machine is already paused")
+
+        return f"{self.status}:{self.time}: {self.name}\n"
+
+
+    def finish(self):
+        try:
+            if self.status != 'active' and self.status !='pause':
+                self.status = 'finish'
+                self.time = datetime.now().strftime('%H :%M : %S')
+                Machine.counter += 1
+                print(Fore.YELLOW + "machine finishied!")
+
+            else :
+                raise ValueError
+
+        except: 
+            print("machine is already finish")
+
+        return f"{self.status}:{self.time}: {self.name}\n "
+
+    def recharge(self):
+        for i in range(10, 101, 10):
+            time.sleep(2)
+            yield f"is charging in level: {i}"
+
+
+
+        
+
 m1 = Machine('akbar')
+m2 = Machine('asghar')
 
 with Report('text.txt') as rp:
     rp.write(m1.start())
+    rp.write(m2.start())
+    rp.write(m1.pause())
+    rp.write(m2.finish())
+    
+
+print(next(m1.recharge()))
+
+
+
 
     # m1 = Machine('akbar')
     # m1.start()
